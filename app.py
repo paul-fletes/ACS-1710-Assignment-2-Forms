dbe60f04f458477b9e8245472f936b6fa74bbf45
Paul Fletes <paul.fletes@gmail.com>
2022-11-03 17:58:51 -0500

    update /froyo route with form

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import random
 
 app = Flask(__name__)
 
+
 def sort_letters(message):
     """A helper method to sort the characters of a string in alphabetical order
     and return the new string."""
@@ -14,25 +15,36 @@ def homepage():
     """A homepage with handy links for your convenience."""
     return render_template('home.html')
 
+
 @app.route('/froyo')
 def choose_froyo():
     """Shows a form to collect the user's Fro-Yo order."""
-    pass
+    return """
+    <form action="/froyo_results" method="GET">
+        What is your favorite Fro-Yo flavor? <br>
+        <input type='text' name='flavor'> <br>
+        <input type='submit' value='Submit!'>
+    </form>
+    """
+
 
 @app.route('/froyo_results')
 def show_froyo_results():
     """Shows the user what they ordered from the previous page."""
     pass
 
+
 @app.route('/favorites')
 def favorites():
     """Shows the user a form to choose their favorite color, animal, and city."""
     pass
 
+
 @app.route('/favorites_results')
 def favorites_results():
     """Shows the user a nice message using their form results."""
     pass
+
 
 @app.route('/secret_message')
 def secret_message():
@@ -40,10 +52,12 @@ def secret_message():
     the POST method to keep it a secret!"""
     pass
 
+
 @app.route('/message_results', methods=['POST'])
 def message_results():
     """Shows the user their message, with the letters in sorted order."""
     pass
+
 
 @app.route('/calculator')
 def calculator():
@@ -62,6 +76,7 @@ def calculator():
         <input type="submit" value="Submit!">
     </form>
     """
+
 
 @app.route('/calculator_results')
 def calculator_results():
@@ -84,10 +99,12 @@ HOROSCOPE_PERSONALITIES = {
     'pisces': 'Imaginative and sensitive'
 }
 
+
 @app.route('/horoscope')
 def horoscope_form():
     """Shows the user a form to fill out to select their horoscope."""
     return render_template('horoscope_form.html')
+
 
 @app.route('/horoscope_results')
 def horoscope_results():
@@ -105,11 +122,12 @@ def horoscope_results():
 
     context = {
         'horoscope_sign': horoscope_sign,
-        'personality': users_personality, 
+        'personality': users_personality,
         'lucky_number': lucky_number
     }
 
     return render_template('horoscope_results.html', **context)
+
 
 if __name__ == '__main__':
     app.config['ENV'] = 'development'
